7e98ab94a0a932037fe896f8d7a436ff11e01799
Hakimuddin Hirani <hakimuddinhirani@gmail.com>
2026-07-03 16:51:05 +0530

    Delete backend directory

--- backend/stellar_evolution/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Backend package for the Stellar Evolution simulation."""
-
-from .models import Composition, SimulationConfig
-from .solver import simulate_structure
-
-__all__ = ["Composition", "SimulationConfig", "simulate_structure"]
